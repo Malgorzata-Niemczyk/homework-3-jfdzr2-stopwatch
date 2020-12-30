f1b5e4747c0ec35fe4ea72a73efbb2aeedf325ed
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-30 21:12:58 +0100

    grab the button elements using querySelector and store them in variables

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
 console.log('Good luck!')
+
+const startBtn = document.querySelector('#start');
+const nextBtn = document.querySelector('#next');
+const stoptBtn = document.querySelector('#stop');
+const pauseBtn = document.querySelector('#pause');
+const saveBtn = document.querySelector('#save');
+const resetBtn = document.querySelector('#reset');
