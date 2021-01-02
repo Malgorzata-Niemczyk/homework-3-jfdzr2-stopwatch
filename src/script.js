628c0a69f2ce81f768e00a5152f6bec6ce634c6f
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-01-02 19:18:08 +0100

    add variables that store the minutes, seconds and miliseconds and create a timerState variable that store the timer values; add click event on the start, stop and reset buttons and add callback functions to them

--- src/script.js
+++ src/script.js
@@ -6,3 +6,32 @@ const stoptBtn = document.querySelector('#stop');
 const pauseBtn = document.querySelector('#pause');
 const saveBtn = document.querySelector('#save');
 const resetBtn = document.querySelector('#reset');
+const timerDisplay = document.querySelector('#timer-display');
+
+let min = 00;
+let sec = 00;
+let milisec = 00;
+let timerState; // to store the timer values
+
+
+// Starting the timer
+function startTimer() {
+    if (!timerState) {
+        timerState = setInterval(formatTimer, 10)
+    };  
+};
+
+function formatTimer() {
+    timerDisplay.innerHTML = `${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}:${milisec < 10 ? `0${milisec}` : milisec}`;
+    milisec++;
+
+    if (milisec === 99) {
+        milisec = 00;
+        sec++;
+    }
+};
+
+
+startBtn.addEventListener('click', startTimer);
+stopBtn.addEventListener('click', stopTimer);
+resetBtn.addEventListener('click', resetTimer);
