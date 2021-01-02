2ae37d97d1f20240d22a093e556a2a23e3d2eba7
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-01-02 20:01:26 +0100

    update the values of the min, sec and milisec; within the formatTimer function add an 'if' statement that increments the value of the min variable when the sec variable's value is equal to 59 and at the same time it clears the value of the sec variable to zero; add the stopTimer function that clears the Interval

--- src/script.js
+++ src/script.js
@@ -2,35 +2,44 @@ console.log('Good luck!')
 
 const startBtn = document.querySelector('#start');
 const nextBtn = document.querySelector('#next');
-const stoptBtn = document.querySelector('#stop');
+const stopBtn = document.querySelector('#stop');
 const pauseBtn = document.querySelector('#pause');
 const saveBtn = document.querySelector('#save');
 const resetBtn = document.querySelector('#reset');
 const timerDisplay = document.querySelector('#timer-display');
 
-let min = 00;
-let sec = 00;
-let milisec = 00;
+let min = 0;
+let sec = 0;
+let milisec = 0;
 let timerState; // to store the timer values
-
 
 // Starting the timer
 function startTimer() {
     if (!timerState) {
-        timerState = setInterval(formatTimer, 10)
-    };  
+        timerState = setInterval(formatTimer, 10);
+    }
+        
 };
 
 function formatTimer() {
-    timerDisplay.innerHTML = `${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}:${milisec < 10 ? `0${milisec}` : milisec}`;
-    milisec++;
-
+    timerDisplay.innerHTML = (min < 10 ? `0${min}` : min) + ":" + (sec < 10 ? `0${sec}` : sec) + ":" + (milisec < 10 ? `0${milisec}` : milisec);
+    milisec++
+    
     if (milisec === 99) {
-        milisec = 00;
+        milisec = 0;
         sec++;
-    }
+    };
+
+    if (sec === 59) {
+        sec = 0;
+        min++;
+    };
 };
 
+function stopTimer() {
+    clearInterval(timerState);
+    timerState = false;
+};
 
 startBtn.addEventListener('click', startTimer);
 stopBtn.addEventListener('click', stopTimer);
