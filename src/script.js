edececdca9c016dabdae7d706e9216ffe6239aed
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-01-02 21:37:07 +0100

    grab the ul tag with an id of next-result-display and store it in a nextResultDisplay variable; add the click event on the nextBtn and trigger the displayNextResult function, this function creates a new li tag that is to display new time and appends it to the ul tag with an id of next-result-display

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ const pauseBtn = document.querySelector('#pause');
 const saveBtn = document.querySelector('#save');
 const resetBtn = document.querySelector('#reset');
 const timerDisplay = document.querySelector('#timer-display');
+const nextResultDisplay = document.getElementById('next-result-display');
 
 let min = 0;
 let sec = 0;
@@ -58,7 +59,17 @@ function resetTimer() {
     min = 0;
     sec = 0;
     milisec = 0;
+    
     timerDisplay.innerHTML = (min < 10 ? `0${min}` : min) + ":" + (sec < 10 ? `0${sec}` : sec) + ":" + (milisec < 10 ? `0${milisec}` : milisec);
+
+    nextResultDisplay.innerHTML = '';
+}
+
+function displayNextResult() {
+    let liItem = document.createElement('li');
+    liItem.setAttribute('class', 'next-result-item');
+    liItem.innerHTML = (min < 10 ? `0${min}` : min) + ":" + (sec < 10 ? `0${sec}` : sec) + ":" + (milisec < 10 ? `0${milisec}` : milisec);
+    nextResultDisplay.appendChild(liItem);
 }
 
 
@@ -66,3 +77,4 @@ startBtn.addEventListener('click', startTimer);
 stopBtn.addEventListener('click', stopTimer);
 resetBtn.addEventListener('click', resetTimer);
 pauseBtn.addEventListener('click', pauseTimer);
+nextBtn.addEventListener('click', displayNextResult);
