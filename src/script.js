5db36608c15198aab880bad0c272eda2e26077b3
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-01-04 14:40:39 +0100

    add click event on the saveBtn element; upon clicking saveBtn the saveTimerResults function is triggered; within the saveTimerResults function add a timerResultsArr variable that stores an empty array, use localStorage.setItem method to set up the localStorage that is to save the timer results and push them into an empty array, add clearing the timer display upon clickin the saveBtn

--- src/script.js
+++ src/script.js
@@ -83,9 +83,24 @@ function displayNextResult() {
     }
 };
 
+function saveTimerResults() {
+    let timerResultsArr = [];
+
+    timerResultsArr.push(timerDisplay.innerText);
+    localStorage.setItem('timerResults', JSON.stringify(timerResultsArr));
+
+    // clearing the timer display and the other timer results upon clicking the save button
+    min = 0;
+    sec = 0;
+    milisec = 0;
+    timerDisplay.innerHTML = (min < 10 ? `0${min}` : min) + ":" + (sec < 10 ? `0${sec}` : sec) + ":" + (milisec < 10 ? `0${milisec}` : milisec);
+    nextResultDisplay.innerHTML = '';
+};
+
 
 startBtn.addEventListener('click', startTimer);
 stopBtn.addEventListener('click', stopTimer);
 resetBtn.addEventListener('click', resetTimer);
 pauseBtn.addEventListener('click', pauseTimer);
 nextBtn.addEventListener('click', displayNextResult);
+saveBtn.addEventListener('click', saveTimerResults);
